1efe18e47eea101c364e24c7162f6ebd5699445e
andreamorosi <andreamorosi94@gmail.com>
2023-10-14 00:13:06 +0200

    feat: fixed fusejs results data

--- public/js/main.js
+++ public/js/main.js
@@ -211,7 +211,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         distance: 100,
         maxPatternLength: 32,
         minMatchCharLength: 1,
-        keys: ['title', 'author']
+        keys: ['title', 'author'],
     };
     
     var fuse = new Fuse(projects, options);
@@ -223,25 +223,30 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
                 if(keyword != "") {
                     var result = fuse.search(keyword);
+                    
                     document.getElementById('item-wrapper').innerHTML = "";
-                    printItems(result);
+                    printItems(result, false);
                 } else if(keyword == "") {
                     document.getElementById('item-wrapper').innerHTML = "";
-                    printItems(projects);
+                    printItems(projects, true);
                 }
             }, 150);
         }
     );
 
-    function printItems (library) {
+    function printItems (library, mode) {
+        var libraryResults = []
         for(var i = 0; i < library.length; i++) {
-
-            if(library[i].title !== "") {
+            if(!mode) {
+                libraryResults[i] = library[i].item
+            } else {
+                libraryResults[i] = library[i]
+            }
+            if(libraryResults[i].title !== "") {
                 var htmltags = '';
-
-                for(var j = 0; j < library[i].tags.length; j++) {
-                    if(library[i].tags[j] !== "") {
-                        htmltags += '<span class="item__label">'+library[i].tags[j]+'</span>';
+                for(var j = 0; j < libraryResults[i].tags.length; j++) {
+                    if(libraryResults[i].tags[j] !== "") {
+                        htmltags += '<span class="item__label">'+libraryResults[i].tags[j]+'</span>';
                     }
                 }
 
@@ -252,24 +257,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     <div class="item__inner">
                         <div class="item__main">
                             <header class="item__head">
-                                <h3 class="item__title">`+library[i].title+`</h3>
+                                <h3 class="item__title">`+libraryResults[i].title+`</h3>
                             </header>
 
                             <div class="item__body">
                                 <div class="item__labels">` + htmltags + `</div>
                                 <p class="item__descr">
-                                    `+library[i].description+`
+                                    `+libraryResults[i].description+`
                                 </p>
                             </div>
                         </div>
-                        <picture class="item__logo" style="` + (library[i].img ? 'display: block;' : 'display: none;') + `">
-                            <img src="` + library[i].img + `" alt="" class="item__logo__img">
+                        <picture class="item__logo" style="` + (libraryResults[i].img ? 'display: block;' : 'display: none;') + `">
+                            <img src="` + libraryResults[i].img + `" alt="" class="item__logo__img">
                         </picture>
                     </div>
 
                     <div class="item__foot">
-                        <a href="`+library[i].docs+`" class="item__foot__btn">Documentation</a>
-                        <a href="`+library[i].github+`" class="item__foot__link">GitHub <span class="item__link__icon"></span></a>
+                        <a href="`+libraryResults[i].docs+`" class="item__foot__btn">Documentation</a>
+                        <a href="`+libraryResults[i].github+`" class="item__foot__link">GitHub <span class="item__link__icon"></span></a>
                     </div>
 
                 </article><!--/item-->
@@ -279,6 +284,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         }
     }
 
-    printItems(projects);
+    printItems(projects, true);
 
 }, false);
